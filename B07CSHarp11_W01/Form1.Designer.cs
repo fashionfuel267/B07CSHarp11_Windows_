ae707dee8c720dd3ac1fe2836547e12390fe478c
Shahanaj <fashionfuel267@gmail.com>
2024-11-17 12:54:18 +0600

    Add project files.

---
+++ B07CSHarp11_W01/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace B07CSHarp11_W01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtName.ForeColor = System.Drawing.Color.Black;
+            this.txtName.Location = new System.Drawing.Point(191, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(162, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            this.btnGet.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGet.Location = new System.Drawing.Point(236, 244);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(108, 36);
+            this.btnGet.TabIndex = 6;
+            this.btnGet.Text = "Save";
+            this.btnGet.UseVisualStyleBackColor = false;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(191, 307);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(656, 179);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            this.dataGridView1.DpiChangedAfterParent += new System.EventHandler(this.dataGridView1_DpiChangedAfterParent);
+            // 
+            // txtContact
+            // 
+            this.txtContact.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtContact.ForeColor = System.Drawing.Color.Black;
+            this.txtContact.Location = new System.Drawing.Point(191, 116);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(162, 20);
+            this.txtContact.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contact";
+            // 
+            // txtEMail
+            // 
+            this.txtEMail.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtEMail.ForeColor = System.Drawing.Color.Black;
+            this.txtEMail.Location = new System.Drawing.Point(538, 41);
+            this.txtEMail.Name = "txtEMail";
+            this.txtEMail.Size = new System.Drawing.Size(162, 20);
+            this.txtEMail.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(404, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtAddress.ForeColor = System.Drawing.Color.Black;
+            this.txtAddress.Location = new System.Drawing.Point(538, 116);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(162, 20);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(404, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Address";
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtId.ForeColor = System.Drawing.Color.Black;
+            this.txtId.Location = new System.Drawing.Point(191, 175);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(162, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(57, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Id";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRemove.Location = new System.Drawing.Point(556, 244);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(108, 36);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnClear.Location = new System.Drawing.Point(716, 244);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(108, 36);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear ";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnUpdate.Location = new System.Drawing.Point(396, 244);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(108, 36);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Visible = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 533);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Personal INformation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ B07CSHarp11_W01/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B07CSHarp11_W01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Doctor> listdoctors=new List<Doctor>();
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+          Doctor doctor = new Doctor();
+            doctor.Name=txtName.Text;
+            doctor.Id=int.Parse( this.txtId.Text);
+            doctor.Address=txtAddress.Text;
+            doctor.Contact=txtContact.Text;
+            doctor.Email = this.txtEMail.Text;
+            if(listdoctors.Any(a=>a.Id==doctor.Id))
+            {
+                MessageBox.Show($"{doctor.Id} already exist.");
+            }
+            else
+            {
+                listdoctors.Add(doctor);
+                LoadData();
+                ClearControl();
+            }
+            
+
+           
+            //MessageBox.Show($"Your Company Name is {this.txtName.Text}","Your Name",MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
+
+        }
+        private void LoadData()
+        {
+            this.dataGridView1.DataSource = null;
+         this.dataGridView1.DataSource = listdoctors;
+            this.dataGridView1.Refresh();
+
+
+        }
+        int selectedindex;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedindex = this.dataGridView1.Rows[e.RowIndex].Index;
+           
+        }
+        private void ClearControl()
+        {
+            txtName.Text="";
+              this.txtId.Text="";
+          txtAddress.Text  = "";
+          txtContact.Text = "";
+            this.txtEMail.Text = "";
+
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            listdoctors.RemoveAt(selectedindex);
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = listdoctors;
+            this.dataGridView1.Refresh();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearControl();
+        }
+
+        private void dataGridView1_DpiChangedAfterParent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            selectedindex = this.dataGridView1.SelectedRows[0].Index;
+            var selectedItem = dataGridView1.SelectedRows;
+           this.txtName.Text = selectedItem[0].Cells[0].Value.ToString();
+          this.txtId.Text = selectedItem[0].Cells[1].Value.ToString();
+            this.txtEMail.Text = selectedItem[0].Cells[2].Value.ToString();
+            this.txtAddress.Text = selectedItem[0].Cells[3].Value.ToString();
+            this.txtContact.Text = selectedItem[0].Cells[4].Value.ToString();
+            this.txtId.Enabled = false;
+            this.btnUpdate.Visible = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id =int.Parse( this.txtId.Text);
+           var doctor= this.listdoctors.Find(d=>d.Id== id);
+            doctor.Name=this.txtName.Text;
+            doctor.Email = this.txtAddress.Text;
+            doctor.Contact= this.txtContact.Text;
+            doctor.Address= this.txtAddress.Text;
+            doctor.Id= id;
+            listdoctors.RemoveAt(selectedindex);
+            listdoctors.Insert(selectedindex,doctor);
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = listdoctors;
+            this.dataGridView1.Refresh();
+
+        }
+    }
+}
